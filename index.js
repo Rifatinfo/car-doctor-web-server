43e49be4f06e417e3bcc3abf80f5e59355a597e5
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-10-11 23:15:55 +0600

    indibitual data collection add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000; // Port set to 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config()
 // Middleware
@@ -33,7 +33,17 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
- 
+    app.get('/services/:id', async (req,res) => {
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id)};
+
+     const option  = {
+      projection: { title : 1 , price:1 , service_id : 1}
+     }
+
+      const result = await serviceCollection.findOne(query,option);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
