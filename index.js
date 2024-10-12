e97a9ebd368ac6d5f4dec0ac2cccfd0a0012b00a
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-10-12 13:12:09 +0600

    booking data add

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000; // Port set to 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-require('dotenv').config()
+require('dotenv').config();
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -28,20 +28,41 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db("carDoctor").collection("services");
-    app.get('/services', async(req, res) => {
+    const bookingCollection = client.db("carDoctor").collection("bookings");
+
+
+    app.get('/services', async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-    app.get('/services/:id', async (req,res) => {
+    app.get('/services/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id : new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
 
-     const option  = {
-      projection: { title : 1 , price:1 , service_id : 1}
-     }
+      const option = {
+        projection: { title: 1, price: 1, service_id: 1, img: 1 }
+      }
 
-      const result = await serviceCollection.findOne(query,option);
+      const result = await serviceCollection.findOne(query, option);
+      res.send(result);
+    })
+
+    // bookings 
+    app.get('/bookings', async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.body.email }
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.post('/bookings', async (req, res) => {
+      const bookings = req.body;
+      console.log(bookings);
+      const result = await bookingCollection.insertOne(bookings);
       res.send(result);
     })
 
