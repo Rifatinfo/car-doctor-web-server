37cbd7f9396d1a7b7bef92442f1b3d9b8d06a5af
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-10-12 19:22:31 +0600

    last part

--- index.js
+++ index.js
@@ -53,16 +53,37 @@ async function run() {
       console.log(req.query.email);
       let query = {};
       if (req.query?.email) {
-        query = { email: req.body.email }
+        query = { email: req.query.email }; // Fix: use req.query.email for GET requests
       }
       const result = await bookingCollection.find(query).toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     app.post('/bookings', async (req, res) => {
       const bookings = req.body;
       console.log(bookings);
       const result = await bookingCollection.insertOne(bookings);
+      res.send(result);
+    })
+
+    app.delete('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query)
+      res.send(result);
+    })
+
+    app.patch('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updateDoc  = {
+        $set : {
+          status : updatedBooking.status
+        },
+      };
+      const result = await bookingCollection.updateOne(filter,updateDoc);
       res.send(result);
     })
 
